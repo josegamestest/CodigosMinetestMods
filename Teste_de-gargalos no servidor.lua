ab10c3e5e478b526eb30b77b9a6ba3ff4288b3c5
Jose Anastacio <76887321+josegamestest@users.noreply.github.com>
2022-05-14 10:34:02 -0300

    Update Teste_de-gargalos no servidor.lua

--- Teste_de-gargalos no servidor.lua
+++ Teste_de-gargalos no servidor.lua
@@ -1,3 +1,3 @@
 --test gargalo do codigo
-local testa = minetest.get_us_time()
-minetest.chat_send_all(string.format("elapsed time: %g ms", (minetest.get_us_time() - testa) / 1000))
+local jose_test = minetest.get_us_time()
+minetest.chat_send_all(string.format("elapsed time: %g ms", (minetest.get_us_time() - jose_test) / 1000))
