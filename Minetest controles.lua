0f12f776975a6165ee945b23182189759e171dab
Jose Anastacio <76887321+josegamestest@users.noreply.github.com>
2024-02-10 23:44:14 -0300

    Create Minetest controles.lua

---
+++ Minetest controles.lua
@@ -0,0 +1,10 @@
+controles do jogador que podem se acessados no minetest usando a função get_player_control()
+Alguns dos campos comuns disponíveis na tabela de controles:
+    up: Verdadeiro se a tecla para frente estiver pressionada.
+    down: Verdadeiro se a tecla para trás estiver pressionada.
+    left: Verdadeiro se a tecla para a esquerda estiver pressionada.
+    right: Verdadeiro se a tecla para a direita estiver pressionada.
+    jump: Verdadeiro se a tecla de pulo estiver pressionada.
+    sneak: Verdadeiro se a tecla de agachamento/andar devagar estiver pressionada.
+    LMB (Left Mouse Button): Verdadeiro se o botão esquerdo do mouse estiver pressionado.
+    RMB (Right Mouse Button): Verdadeiro se o botão direito do mouse estiver pressionado.
